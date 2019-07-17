dea060c1afc44cd6fe5f32dda018c45c600275e8
yangmingming <yangmingming@dowsure.com>
2019-07-17 14:09:42 +0800

    dabao

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,4 @@
-const BASE_URL='http://192.168.1.137:8082'  //(此处书写需要复用的url，注意写上端口号)
-// const BASE_URL='http://test.api.dousha8ao.com'
-const IMG_URL='http://192.168.1.137:8082'  // 图片地址
-const IMG_URL_show='http://192.168.1.137:8082'  // 图片显示地址
-
+const BASE_URL='http://propred.api.dowsure.com/freightsure'  //接口地址
+const IMG_URL='http://propred.api.dowsure.com/freightsure'  // 图片地址
+const IMG_URL_show='http://propred.api.dowsure.com'  // 图片显示地址
 export{ BASE_URL, IMG_URL, IMG_URL_show}
